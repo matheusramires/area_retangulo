1b072b101ccfddf3b535e01549cb6714ef45fe93
matheusramires <80134011+matheusramires@users.noreply.github.com>
2021-03-10 23:07:39 -0300

    Add files via upload

---
+++ retangulo.py
@@ -0,0 +1,6 @@
+altura = float(input('digite o valor da altura em cm: '))
+base = float(input('digite o valor da base em cm: '))
+
+area = base * altura
+
+print('Area do retangulo é: '+str(area)+'cm²')
